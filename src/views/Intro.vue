54edb5c2c23340d907927a7a18e317128693700b
frankfang <frankfang1990@gmail.com>
2020-09-03 14:47:23 +0800

    test

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,3 +1,20 @@
 <template>
 <h1>介绍</h1>
 </template>
+
+<script>
+export default {
+  props: {
+    y: String
+  },
+  data() {
+    return {
+      x: 'hi'
+    }
+  }
+}
+</script>
+
+<md>
+# hi
+</md>
