7f48905ee5fa5f19e3d7c3fe769d95253e11b553
frankfang <frankfang1990@gmail.com>
2020-09-08 11:13:42 +0800

    remove custom blocks

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,4 @@
-// import markdown from 'rollup-plugin-markdown';
-
 export default {
   vueCustomBlockTransforms: {
-    md: (x) => {
-      console.log(x);
-      // return transformed code
-      return `
-      export default Comp => {
-        console.log(Comp);
-        Object.assign(Comp.props, {md: {type:String, default:"hi"}})
-        Comp.i18n = ${JSON.stringify('hi' || {})}
-      }`.trim()
-    },
   },
 };
